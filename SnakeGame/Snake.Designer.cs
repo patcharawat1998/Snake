eab020e573740b0d644d5d4295a0c20603c33dc2
PATCHARAWAT SUKRUK <patcharawat_sukruk@cmu.ac.th>
2017-10-24 17:30:06 +0700

    First Commit
    First Commit

---
+++ SnakeGame/Snake.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SnakeGame
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblScoreText = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.chbDebug = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(69, 314);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(318, 69);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start New Game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblScoreText
+            // 
+            this.lblScoreText.AutoSize = true;
+            this.lblScoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreText.Location = new System.Drawing.Point(118, 14);
+            this.lblScoreText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScoreText.Name = "lblScoreText";
+            this.lblScoreText.Size = new System.Drawing.Size(179, 64);
+            this.lblScoreText.TabIndex = 1;
+            this.lblScoreText.Text = "Score";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(255, 117);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(44, 47);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "0";
+            // 
+            // chbDebug
+            // 
+            this.chbDebug.AutoSize = true;
+            this.chbDebug.Location = new System.Drawing.Point(69, 278);
+            this.chbDebug.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chbDebug.Name = "chbDebug";
+            this.chbDebug.Size = new System.Drawing.Size(83, 24);
+            this.chbDebug.TabIndex = 3;
+            this.chbDebug.Text = "Debug";
+            this.chbDebug.UseVisualStyleBackColor = true;
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 402);
+            this.Controls.Add(this.chbDebug);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblScoreText);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Snake";
+            this.Text = "SnakeUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Snake_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Snake_FormClosed);
+            this.Load += new System.EventHandler(this.Snake_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblScoreText;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.CheckBox chbDebug;
+    }
+}
